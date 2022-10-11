2d867757c3577c25ef0cf84a6ba1217b8485923b
Peter Cox <pcox@contentformula.com>
2022-10-11 16:48:32 +0100

    Initial commit

---
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { FilePicker, IFilePickerResult } from '@pnp/spfx-controls-react/lib/FilePicker';
+
+export interface IHelloWorldProps {
+  context: any;
+}
+
+export const HelloWorld = (props: IHelloWorldProps) => {
+  return (
+    <div>
+      <FilePicker
+        context={props.context}
+        buttonIcon="FileImage"
+        buttonLabel="Select file"
+        hidden
+        hideWebSearchTab
+        hideStockImages
+        hideOrganisationalAssetTab
+        hideOneDriveTab
+        hideLinkUploadTab
+        hideLocalMultipleUploadTab
+        hideLocalUploadTab
+        hideRecentTab
+        onSave={() => alert("hi")}
+        // onSave={(fppr: IFilePickerResult[]) => {
+        //   addCardLink(fppr);
+        //   setIsLinkPickerVisible(false);
+        //   props.onLinkPickerDismiss();
+        // }}
+        // context={context}
+        // onChange={(fppr: IFilePickerResult[]) => {
+        //   addCardLink(fppr);
+        //   setIsLinkPickerVisible(false);
+        //   props.onLinkPickerDismiss();
+        // }}
+        // isPanelOpen={isLinkPickerVisible}
+        // onCancel={() => {
+        //   setIsLinkPickerVisible(false);
+        //   props.onLinkPickerDismiss();
+        // }}
+        includePageLibraries
+  />
+
+    </div>
+   
+  );
+
+}
